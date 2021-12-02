e5e82b8a6f66145d1d03200609c742796fb9e955
Steven <stevengf14@gmail.com>
2021-12-02 17:41:57 -0500

    CRUD

--- src/main/java/ec/com/learning/client/web/InitController.java
+++ src/main/java/ec/com/learning/client/web/InitController.java
@@ -2,12 +2,14 @@
 package ec.com.learning.client.web;
 
 import ec.com.learning.client.dao.PersonDao;
+import ec.com.learning.client.domain.Person;
 import ec.com.learning.client.service.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -22,5 +24,29 @@ public class InitController {
         log.info("Executing Spring MVC controller");
         model.addAttribute("people", people);
         return "index";
+    }
+    
+    @GetMapping("/add")
+    public String add(Person person){
+        return "edit";
+    }
+    
+    @PostMapping("/save")
+    public String save(Person person){
+        personService.save(person);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/edit/{idPerson}")
+    public String edit(Person person, Model model){
+        person = personService.findPerson(person);
+        model.addAttribute("person", person);
+        return "edit";
+    }
+    
+    @GetMapping("/delete")
+    public String delete(Person person){
+        personService.delete(person);
+        return "redirect:/";
     }
 }
